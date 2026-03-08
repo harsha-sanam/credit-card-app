ed0b0e1fdd161313920ba5120551938e9bc5f597
harsha-sanam <34478540+harsha-sanam@users.noreply.github.com>
2026-03-07 17:33:08 -0800

    bug fix

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://credit-card-app-8ykm.onrender.com/', // Set your production API URL
+  apiUrl: 'https://credit-card-app-8ykm.onrender.com', // Set your production API URL
   googleClientId: '1080739292172-4tot1riueigiarodn90sce55ou774ub8.apps.googleusercontent.com' // Set your production Google Client ID
 };
